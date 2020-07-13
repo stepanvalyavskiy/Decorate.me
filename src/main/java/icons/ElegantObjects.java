b6b015b73106abd6f5a79dd3d691d6ed33ca681c
Stepan Valiavskii <Stepanvalyavskiy@gmail.com>
2020-07-13 11:57:06 +0300

    CACTOOS no more in use. In completion list: default nodes/class.svg is in use. New handmade plugin logo. The plugin is renamed to "Elegant IDE"

--- src/main/java/icons/ElegantObjects.java
+++ src/main/java/icons/ElegantObjects.java
@@ -1,9 +1,11 @@
 package icons;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.util.IconLoader;
 
 import javax.swing.*;
 
 public interface ElegantObjects {
    Icon CACTUS = IconLoader.getIcon("/icons/cactus@2x.svg");
+   Icon CLASS = AllIcons.Nodes.Class;
 }
